f6af847fcb8d3c598839efae4654f7e749a2799d
unknown <es24-csepanyi@ipari.vein.hu>
2025-12-26 17:32:58 +0100

    vite.config.js pici javítás

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         // Most már a /backend mappára kell mutatni!
-        target: 'http://localhost/lego_final2/legora/backend', 
+        target: 'http://localhost/legora/backend', 
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
