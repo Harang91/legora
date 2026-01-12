653db3b4652b922c2ce8a14a680434e6c164855c
András Marton <andras.marton.dev@gmail.com>
2026-01-12 16:22:25 +0100

    Update index.php

--- backend/index.php
+++ backend/index.php
@@ -1,8 +1,14 @@
 <?php
-include "./sql_fuggvenyek.php";
-    $metodus = $_SERVER["REQUEST_METHOD"];
-    $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-    $uri = explode("/", $uri);
-    $bodyAdatok = json_decode(file_get_contents("php://input"), true);
 
-?>
+// Alap backend inicializálás: segédfüggvények betöltése
+include "./sql_fuggvenyek.php";
+
+// HTTP metódus beolvasása (GET, POST, stb.)
+$metodus = $_SERVER["REQUEST_METHOD"];
+
+// URI feldolgozása (útvonal szegmentálása)
+$uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$uri = explode("/", $uri);
+
+// JSON body beolvasása (POST/PUT esetén)
+$bodyAdatok = json_decode(file_get_contents("php://input"), true);
