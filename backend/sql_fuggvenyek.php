37a51d51b0cd26ced1d55653e3868357c32d4767
András Marton <andras.marton.dev@gmail.com>
2026-01-12 16:26:16 +0100

    Update sql_fuggvenyek.php

--- backend/sql_fuggvenyek.php
+++ backend/sql_fuggvenyek.php
@@ -1,32 +1,33 @@
 <?php
-    //Adatok lekérése az adatbázisból:
-    function adatokLekerese($muvelet) {
-        $db = new mysqli("localhost", "root", "", "legora");
-        if ($db->connect_errno != 0) {
-            return $db->connect_error;
-        }
-        $eredmeny = $db->query($muvelet);
 
-        if ($db->errno != 0) {
-            return $db->error;
-        }
-
-        return ($eredmeny->num_rows > 0) ? $eredmeny->fetch_all(MYSQLI_ASSOC) : [] ;
+// Adatok lekérése az adatbázisból (SELECT műveletek)
+function adatokLekerese($muvelet)
+{
+    $db = new mysqli("localhost", "root", "", "legora");
+    if ($db->connect_errno != 0) {
+        return $db->connect_error;
     }
 
-
-    //INSERT, UPDATE, DELETE típusú SQL műveletekhez:
-    function adatokValtoztatasa($muvelet){
-        $db = new mysqli("localhost", "root", "", "legora");
-        if ($db->connect_errno != 0) {
-            return $db->connect_error;
-        }
-
-        $db->query($muvelet);
-
-        if ($db->errno != 0) {
-            return $db->error;
-        }
-        return $db->affected_rows > 0 ? true : false;
+    $eredmeny = $db->query($muvelet);
+    if ($db->errno != 0) {
+        return $db->error;
     }
-?>
+
+    return ($eredmeny->num_rows > 0) ? $eredmeny->fetch_all(MYSQLI_ASSOC) : [];
+}
+
+// INSERT, UPDATE, DELETE típusú SQL műveletek
+function adatokValtoztatasa($muvelet)
+{
+    $db = new mysqli("localhost", "root", "", "legora");
+    if ($db->connect_errno != 0) {
+        return $db->connect_error;
+    }
+
+    $db->query($muvelet);
+    if ($db->errno != 0) {
+        return $db->error;
+    }
+
+    return $db->affected_rows > 0 ? true : false;
+}
